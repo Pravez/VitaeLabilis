6cdfaf1cd68ec101e50eeb11e12e7b74bf21dae1
pravez <paul.ravezb@gmail.com>
2017-05-27 15:38:27 +0200

    Changed a bit script to consider size and iterations

--- experiences/lancer-expe.sh
+++ experiences/lancer-expe.sh
@@ -4,8 +4,11 @@ export OMP_NUM_THREADS
 ITE=$(seq 10) # nombre de mesures
   
 THREADS=$(seq 2 2 24) # nombre de threads
+SIZE=$1
+ITERATIONS=$2
+VERSIONS=$(0 1 2 3 4 5)
 
-PARAM="-n -s 4096 -a" # parametres commun à toutes les executions 
+PARAM="-n -a" # parametres commun à toutes les executions 
 
 execute (){
 EXE="./prog $* $PARAM"
@@ -13,10 +16,10 @@ OUTPUT="$(echo $EXE | tr -d ' ')"
 for nb in $ITE; do for OMP_NUM_THREADS in $THREADS; do  echo -n "$OMP_NUM_THREADS " >> $OUTPUT ; $EXE 2>> $OUTPUT; done; done
 }
 
-execute -v 0
-execute -v 1
-execute -v 2
-execute -v 3
+for v in $VERSIONS;
+do
+    execute -v $v -s $SIZE -i $ITERATIONS
+done
 
 
 
