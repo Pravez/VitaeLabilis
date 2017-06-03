cac2e11de45602dbb38aba5a2f42138914549f91
Paul Breton <paubreton@emi.u-bordeaux1.fr>
2017-06-03 09:56:39 +0200

    modified script

--- experiences/lancer-expe.sh
+++ experiences/lancer-expe.sh
@@ -4,24 +4,40 @@ export OMP_NUM_THREADS
 ITE=$(seq 10) # nombre de mesures
   
 THREADS=$(seq 2 2 24) # nombre de threads
-SIZE=$1
-ITERATIONS=(10 100 1000 10000)
-VERSIONS=(0 1 2 3 4 6 7)
+ITERATIONS=("10" "100" "1000" "10000")
+VERSIONS=("2" "3" "4")
+SIZES=("256" "512" "1024" "4096")
 
 PARAM="-n -a" # parametres commun à toutes les executions 
 
 execute (){
-EXE="../prog $* $PARAM"
+cd ../
+EXE="./prog $* $PARAM"
 OUTPUT="$(echo $EXE | tr -d ' ')"
-for nb in $ITE; do for OMP_NUM_THREADS in $THREADS; do  echo -n "$OMP_NUM_THREADS " >> $OUTPUT ; $EXE 2>> $OUTPUT; done; done
+for nb in $ITE; 
+do 
+	for OMP_NUM_THREADS in $THREADS; 
+	do 
+		echo "iteration $nb, thread $OMP_NUM_THREADS"
+		echo -n "$OMP_NUM_THREADS " >> $OUTPUT ;
+		$EXE 2>> experiences/$OUTPUT; 
+	done; 
+done
+cd experiences/
 }
 
-for v in $VERSIONS;
+for v in ${VERSIONS[@]};
 do
-    for it in $ITERATIONS;
+    for it in ${ITERATIONS[@]};
     do
-        execute -v $v -s $SIZE -i $it
-        execute -v $v -s $SIZE -i $it
+		for size in ${SIZES[@]};
+		do
+
+			echo "First size $size, it $it"
+		        execute -v $v -s $size -i $it
+			echo "second size $size, it $it"
+	        	execute -v $v -s $size -i $it
+		done
     done
 done
 
